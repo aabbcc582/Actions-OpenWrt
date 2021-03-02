db50c80b1dd0a062513f26678eeb324ee76d5648
aabbcc582 <59761506+aabbcc582@users.noreply.github.com>
2021-03-02 22:10:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
-sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-Chen'' package/lean/default-settings/files/zzz-default-settings
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='Edge'' package/lean/default-settings/files/zzz-default-settings
